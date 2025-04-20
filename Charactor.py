b6e129c96afff71af43d63cd033e57be5a859ef7
naorex <nao23r@gmail.com>
2025-04-20 14:49:30 +0900

    first commit

---
+++ Charactor.py
@@ -0,0 +1,37 @@
+from module.combat_power_calc import combat_power_calc
+
+class Charactor:
+    """ニケのキャラクターパラメータを定義するクラス
+
+    Ref:
+        https://youtu.be/r6SZJfq0CTQ?si=Qon1cBYqupGoQLG_
+    """
+
+    def __init__(self, params):
+        """コンストラクタ（初期化メソッド）
+        """
+        self.name = params['name']
+        self.combat_power = combat_power_calc(params['hp'],
+                                              params['attack_power'],
+                                              params['defence_power'],
+                                              params['skill_level_one'],
+                                              params['skill_level_two'],
+                                              params['burst_level'])
+
+if __name__ == '__main__':
+
+    params = {
+        'name':'Alice',
+        'hp':2773971,
+        'attack_power':120813,
+        'defence_power':16490,
+        'skill_level_one':10,
+        'skill_level_two':6,
+        'burst_level':9}
+
+    # インスタンスを作成
+    charactor = Charactor(params)
+
+    # キャラの名前と戦闘力を表示
+    print(f'name: {charactor.name},',
+          f'battle_power: {charactor.combat_power}')
---
+++ module/combat_power_calc.py
@@ -0,0 +1,38 @@
+
+
+def combat_power_calc(hp,
+                      attack_power,
+                      defence_power,
+                      skill_level_one,
+                      skill_level_two,
+                      burst_level):
+    """戦闘力計算
+    """
+
+    # クリティカル補正
+    critical_coef = 1.075
+
+    # 体力補正
+    health_coef = defence_power * 100 + hp
+
+    # ステータス補正
+    status_coef = (attack_power * 18 * critical_coef) \
+                + (health_coef * 0.7)
+
+    # スキル補正（アプデ前）
+    skill_coef = skill_level_one * 0.01 \
+                + skill_level_two * 0.01 \
+                + burst_level * 0.02
+
+    # スキル補正（アプデ後）
+    rank_sum_except_adv_code = 84  # => 仮入力
+    rank_sum_of_adv_code = 0       # => 仮入力
+
+    skill_coef = skill_coef \
+            + rank_sum_except_adv_code * 0.0069 \
+            + rank_sum_of_adv_code * 0.0091
+
+    # 戦闘力計算
+    combat_power = round(status_coef * (1.3 + skill_coef) / 100)
+
+    return combat_power
